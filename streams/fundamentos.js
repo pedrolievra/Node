464b77957116d3a782901f251006097868754022
Pedro <pedrolievra@gmail.com>
2023-03-08 19:53:55 -0400

    eu fi esse texto completo
    para poder dar commit
     e eu prefiro desse jeoto
    pq eu posso escreve uma msg para meu botchas

--- streams/fundamentos.js
+++ streams/fundamentos.js
@@ -1,3 +1,40 @@
-//netflix e spotify
 
- 
+import { Readable, Writable, Transform  } from 'node:stream'
+class OneToHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class MultiplyByTenStream extends Writable{
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString())*10)
+        callback()
+    }
+}
+
+class InverseNumberStream extends Transform{
+    _transform(chunck, encoding, callback){
+        const transformed = Number(chunck.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+
+
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
